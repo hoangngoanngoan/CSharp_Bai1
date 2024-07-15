c5317228b19197d5460ae7d16b5c56234240dc31
hoangngoanngoan <ldh2381995@gmail.com>
2024-07-15 20:25:18 +0700

    Thêm hàm Tinhtong, Tinhhieu, Tinhthuong, Tinhtich, Chialaydu

--- CSharp_Bai1/Program.cs
+++ CSharp_Bai1/Program.cs
@@ -1,28 +1,49 @@
-﻿internal class Program
-{
+﻿using System.ComponentModel;
 
+internal class Program
+{
+    internal static int Tinhtong(int i, int j)
+    {
+        return i + j;
+    }
+    internal static int Tinhhieu(int i, int j)
+    {
+        return i - j;
+    }
+    internal static int Tinhtich(int i, int j)
+    {
+        return i * j;
+    }
+    internal static string Tinhthuong(int i, int j)
+    {
+        try
+        {
+            return Convert.ToString(i / j);
+        }catch
+        {
+            return "Khong the chia het cho 0";
+        }
+    }
+    internal static int Chialaydu(int i, int j)
+    {
+        return i % j;
+    }
+    
+    
     private static void Main(string[] args)
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.WriteLine("Xin chào thế giới");
-        Console.WriteLine("Chương trình cộng hai và trừ hai số đơn giản");
-        int i, j, z, x;
+
+        Console.WriteLine("Chương trình tính toán đơn giản");
+        int i, j;
         Console.Write("Nhap i: ");
         i = Convert.ToInt32(Console.ReadLine());
         Console.Write("Nhap j: ");
         j = Convert.ToInt32(Console.ReadLine());
-        z = i + j;
-        x = i - j;
-        Console.WriteLine("Tổng của hai số {0} và số {1} là {2}",i,j,z);
-        Console.WriteLine("Hiệu của hai sô {0} vá số {1} là {2}",i,j,x);
-        try
-        {
-            double thuong;
-            thuong = i / j;
-            Console.WriteLine($"Thuong cua hai so {i} và {j} là {thuong}");
-        }catch {
-            Console.WriteLine("Khong the chia cho 0");
-        }
-
+        Console.WriteLine("Tổng của hai số {0} và số {1} là {2}",i,j,Tinhtong(i,j));
+        Console.WriteLine("Hiệu của hai sô {0} vá số {1} là {2}",i,j,Tinhhieu(i,j));
+        Console.WriteLine($"Thuong cua hai so {i} và {j} là {Tinhthuong(i,j)}");
+        Console.WriteLine($"Tich cua hai so {i} và {j} là {Tinhtich(i, j)}");
+        Console.WriteLine($"Du cua so {i} chia cho {j} là {Chialaydu(i, j)}");
     }
 }
