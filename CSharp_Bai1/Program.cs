c57b2722a5b2bc2242eed0ca94eb54ce71a0ae37
SPC <SPC@18-LAB402>
2024-07-15 19:21:40 +0700

    Add project files.

---
+++ CSharp_Bai1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Xin chào thế giới");
+        Console.WriteLine("Chương trình cộng hai số đơn giản");
+        int i, j, z;
+        Console.Write("Nhap i: ");
+        i = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Nhap j: ");
+        j = Convert.ToInt32(Console.ReadLine());
+        z = i + j;
+        Console.WriteLine("Tổng của hai số {0} và số {1} là {2}",i,j,z);
+
+    }
+}
